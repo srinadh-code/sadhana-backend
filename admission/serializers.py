339e1918b33153cf44db1249f154546f5b5a1a8a
AchheKavya <kavyaachhe@gmail.com>
2026-07-17 16:49:33 +0530

    changesinfrontend

--- admission/serializers.py
+++ admission/serializers.py
@@ -9,7 +9,7 @@ from .models import (
 )
 
 class AdmissionHeroSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
+    # image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = AdmissionHero
@@ -17,21 +17,12 @@ class AdmissionHeroSerializer(serializers.ModelSerializer):
             "id",
             "title",
             "subtitle",
-            "button_text",
-            "image",
-            "image_url",
+            # "button_text",
+            # "image",
+            # "image_url",
             "is_active",
         ]
 
-    def get_image_url(self, obj):
-        request = self.context.get("request")
-
-        if obj.image:
-            if request:
-                return request.build_absolute_uri(obj.image.url)
-            return obj.image.url
-
-        return None
     
 
 
